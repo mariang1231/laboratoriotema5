198bb2f8ca5ace9afd1789e2d617c8a3ea299a51
mariang1231 <mariang1981@gmail.com>
2024-12-07 14:20:43 -0500

    se cambia imagen

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (name && email && message) {
+        alert('Formulario enviado exitosamente. ¡Gracias por contactarnos!');
+    } else {
+        alert('Por favor, completa todos los campos.');
+    }
+});
